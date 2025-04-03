7823dc5f8a31b88c4e6f3baa4d68ae055407fdb6
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-03 19:50:37 +0700

    Configure the middlewares, create initial endpoints, create connection to neon

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+
+import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 
 // Load environment variables
 dotenv.config();
@@ -8,12 +14,41 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Initial routes
-app.get("/", (req, res) => {
-  res.send("Hello from the backend");
-})
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan("dev")); // log the requests
 
-// Start server
-app.listen(PORT, () => {
-  console.log("Server is running on port 5000");
-})
+// Initial routes
+app.use("/api/products", productRoutes);
+
+// Connect to database and start the server
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initDB", error);
+  }
+}
+
+initDB().then(() => {
+  // Start server
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
+});
