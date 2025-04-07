3c3ad6545810fa93897384beef85164047a4cd80
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-07 07:45:30 +0700

    add middlewares and initial product routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,25 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import routes from "./routes/index.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet({
+  contentSecurityPolicy: false,
+}));
+app.use(morgan("dev"));
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello from the backend");
-});
+app.use("/api", routes);
 
 // Listen
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/controllers/productController.js
@@ -0,0 +1,19 @@
+export const getProducts = (req, res) => {
+  res.send("getProducts");
+};
+
+export const getProduct = (req, res) => {
+  res.send("getProduct");
+};
+
+export const createProduct = (req, res) => {
+  res.send("createProduct");
+};
+
+export const updateProduct = (req, res) => {
+  res.send("updateProduct");
+};
+
+export const deleteProduct = (req, res) => {
+  res.send("deleteProduct");
+};
